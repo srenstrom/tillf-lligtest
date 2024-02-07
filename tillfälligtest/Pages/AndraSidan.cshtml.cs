e28edcf85c563b6b4fe808a3284162250c139d29
Susanna Renström <s.renstrom@outlook.com>
2024-02-07 16:22:01 +0100

    Added another page

---
+++ tillfälligtest/Pages/AndraSidan.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tillfälligtest.Pages
+{
+    public class AndraSidanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
