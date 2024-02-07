0992d7d8d96976c3c32772f073656948150e7e5f
Susanna Renström <s.renstrom@outlook.com>
2024-02-07 15:47:00 +0100

    Add project files.

---
+++ tillfälligtest/Pages/Första.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tillfälligtest.Pages
+{
+    public class FörstaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
